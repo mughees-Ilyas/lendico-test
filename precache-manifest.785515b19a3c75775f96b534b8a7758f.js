09018bdc0af1a59e9a2f0dfc93ced17c85903177
mughees <mugheesilyas@live.com>
2019-10-23 19:07:06 +0200

    Updates

---
+++ precache-manifest.785515b19a3c75775f96b534b8a7758f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd2270bb18607075d9adc048e5754e1f",
+    "url": "/lendico-test/index.html"
+  },
+  {
+    "revision": "02c4b96b0ad0d17e52ce",
+    "url": "/lendico-test/static/css/2.ad9eb4b5.chunk.css"
+  },
+  {
+    "revision": "a4c533862d3256d4a52e",
+    "url": "/lendico-test/static/css/main.e8f52ae8.chunk.css"
+  },
+  {
+    "revision": "02c4b96b0ad0d17e52ce",
+    "url": "/lendico-test/static/js/2.126b5bf4.chunk.js"
+  },
+  {
+    "revision": "a4c533862d3256d4a52e",
+    "url": "/lendico-test/static/js/main.3ce457a0.chunk.js"
+  },
+  {
+    "revision": "1b6e866d93a6bf1e09b5",
+    "url": "/lendico-test/static/js/runtime~main.e01de21e.js"
+  }
+]);
