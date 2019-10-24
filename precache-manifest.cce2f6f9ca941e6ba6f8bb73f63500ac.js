fdbbebc608c029b253d45f978f9b4918f825bd05
mughees <mugheesilyas@live.com>
2019-10-24 13:09:03 +0200

    Updates

--- precache-manifest.cce2f6f9ca941e6ba6f8bb73f63500ac.js
+++ precache-manifest.cce2f6f9ca941e6ba6f8bb73f63500ac.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dd2270bb18607075d9adc048e5754e1f",
+    "revision": "1e51e0715c4605cb7937fc948e70c7bd",
     "url": "/lendico-test/index.html"
   },
   {
-    "revision": "02c4b96b0ad0d17e52ce",
+    "revision": "94343a645a65df8b6104",
     "url": "/lendico-test/static/css/2.ad9eb4b5.chunk.css"
   },
   {
-    "revision": "a4c533862d3256d4a52e",
+    "revision": "45c4a3b0239a5aed69cc",
     "url": "/lendico-test/static/css/main.e8f52ae8.chunk.css"
   },
   {
-    "revision": "02c4b96b0ad0d17e52ce",
-    "url": "/lendico-test/static/js/2.126b5bf4.chunk.js"
+    "revision": "94343a645a65df8b6104",
+    "url": "/lendico-test/static/js/2.e8c358ee.chunk.js"
   },
   {
-    "revision": "a4c533862d3256d4a52e",
-    "url": "/lendico-test/static/js/main.3ce457a0.chunk.js"
+    "revision": "45c4a3b0239a5aed69cc",
+    "url": "/lendico-test/static/js/main.aef224e1.chunk.js"
   },
   {
     "revision": "1b6e866d93a6bf1e09b5",
